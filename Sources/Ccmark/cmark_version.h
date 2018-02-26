c02d3f1f1c5d4be4e90bcce841ae7573995ea6b4
Andrew Lees <32634907+Andrew-Lees11@users.noreply.github.com>
2018-02-26 11:15:08 +0000

    Update ccmark to 0.28.3 (#11)
    - and document the procedure for upgrading Cmark in the future in Sources/Ccmark/README.md

--- Sources/Ccmark/cmark_version.h
+++ Sources/Ccmark/cmark_version.h
@@ -1,7 +1,7 @@
 #ifndef CMARK_VERSION_H
 #define CMARK_VERSION_H
 
-#define CMARK_VERSION ((0 << 16) | (25 << 8)  | 2)
-#define CMARK_VERSION_STRING "0.25.2"
+#define CMARK_VERSION ((0 << 16) | (28 << 8)  | 3)
+#define CMARK_VERSION_STRING "0.28.3"
 
 #endif
