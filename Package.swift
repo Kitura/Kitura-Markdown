552c025c839ea79c96ea52cf7228737e48a98ee4
David Jones <djones6dev@gmail.com>
2018-03-01 11:35:30 +0000

    Support Swift 3.1.1 thru 4.1 (#13)

--- Package.swift
+++ Package.swift
@@ -1,8 +1,6 @@
-// swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:3.1
 /**
- * Copyright IBM Corporation 2017
+ * Copyright IBM Corporation 2016, 2017
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,29 +19,12 @@ import PackageDescription
 
 let package = Package(
     name: "KituraMarkdown",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "KituraMarkdown",
-            targets: ["KituraMarkdown"]
-        )
+    targets: [
+        Target(name: "KituraMarkdown", dependencies: ["Ccmark"]),
+        Target(name: "Ccmark", dependencies: [])
     ],
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", from: "2.0.0")
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Ccmark"
-        ),
-        .target(
-            name: "KituraMarkdown",
-            dependencies: ["Ccmark", "KituraTemplateEngine"]
-        ),
-        .testTarget(
-            name: "KituraMarkdownTests",
-            dependencies: ["KituraMarkdown"]
-        )
+        .Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", majorVersion: 1, minor: 7)
     ]
 )
+
